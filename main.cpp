acc1be99202fa6f33d677379e5c44f48b4c5713c
Winton Lim <wintonlim951@gmail.com>
2022-06-06 20:47:51 +1000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "main.h"
+//
+// Created by Winton Lim on 11/05/2022.
+//
+int main(){
+
+    std::cout << "Whats your name: " << std::endl;
+
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by Winton Lim on 11/05/2022.
+//
+
+#ifndef LEARNING_C_PLUSPLUS_MAIN_H
+#define LEARNING_C_PLUSPLUS_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //LEARNING_C_PLUSPLUS_MAIN_H
