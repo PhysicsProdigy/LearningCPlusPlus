c96bef741fdf5a8322d092bad6482b8d474c24db
Winton Lim <wintonlim951@gmail.com>
2022-06-15 09:15:18 +1000

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 #include "main.h"
+
 //
 // Created by Winton Lim on 11/05/2022.
 //
+
+void print(const char *string){
+    std::cout << string;
+}
+void println(const char *string){
+    std::cout << string << std::endl;
+}
+
 int main(){
 
-    std::cout << "Whats your name: " << std::endl;
+    //This is a comment
 
+    /*
+        This is a multilined Comment
+        std::cout << is like a print() statement in java
+        std::cout << sometihng << std::endl; is something like println()
+        Also don't nest comments... Its gonna cause errors
+    */
 
-    return 0;
+    std::cout << "Hello World!: " << std::endl;
+
+    println("My name is: ");
+    print("Physics");
+    print("Prodigy");
 }
